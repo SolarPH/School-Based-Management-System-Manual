4b21e48a9a00149bed6bafd60ebd45aa800f7ae1
SolarPH <mjbenamera.gfx@gmail.com>
2025-03-07 10:14:08 +0800

    BBTAN

---
+++ 2025-BBTANAUAN/01 - Preliminary/Script/RELEASE - PRE - SWIMWEAR.bat
@@ -0,0 +1,10 @@
+@echo off
+
+set filename=02 - Swimwear.xlsx
+set destroot=..\Sheets
+
+@echo on
+ATTRIB -H -R "%destroot%\JUDGE 1\%filename%"
+ATTRIB -H -R "%destroot%\JUDGE 2\%filename%"
+ATTRIB -H -R "%destroot%\JUDGE 3\%filename%"
+pause
